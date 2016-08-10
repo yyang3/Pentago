fdc41d3005ffa196ce7f11979702bbea659e4aa1
yyang3 <yyang3@uw.edu>
2016-08-10 16:54:01 -0700

    need minmax tree and value network implement

---
+++ src/HeuristicAndValueMap.java
@@ -0,0 +1,328 @@
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class HeuristicAndValueMap {
+	/**
+	 * heuristic map of the value on each row, col and diag
+	 */
+	private Map<String, int[]> myHeuristic;
+	/**
+	 * 2d array for the heuristic value of the positions
+	 */
+	private int[][] myValueb;
+	/**
+	 * 2d array for the heuristic value of the positions
+	 */
+	private int[][] myValuew;
+	/**
+	 * heuristic value for 2 in a row
+	 */
+	private static final int TWOINAROW = 1;
+	/**
+	 * heuristic value for 3 in a row
+	 */
+	private static final int THREEINAROW = 10;
+	/**
+	 * heuristic value for 4 in a row
+	 */
+	private static final int FOURINAROW = 100;
+	/**
+	 * heuristic value for 5 in a row
+	 */
+	private static final int FIVEINAROW = 100000000;
+	/**
+	 * helper string to form the heuristic key
+	 */
+	private static final String ROWHEAD = "row ";
+	/**
+	 * helper string to form the heuristic key
+	 */
+	private static final String COLHEAD = "col ";
+	/**
+	 * helper string to form the heuristic key
+	 */
+	private static final String DIAGHEAD = "diag ";
+	
+	
+	public HeuristicAndValueMap(char [][] Input) {
+		myHeuristic = new HashMap<>();
+		myValueb = new int[6][6];
+		myValuew = new int[6][6];
+		updateH(Input);
+	}
+	public Map<String, int[]> getHeuristics () {
+		return myHeuristic;
+	} 
+	/**
+	 * helper method to update the 
+	 * @param Input
+	 */
+	private final void updateH (char [][] Input) {
+		//row
+		for (int i = 0; i < 6; i ++) {
+			checkRow(i, Input);
+		}
+		//col
+		for (int i = 0; i < 6; i ++) {
+			checkCol(i, Input);
+		}
+		checkDiag(0, 0,Input);
+		checkDiag(0, 5,Input);
+	}
+
+	private int checkRow(int i, char[][] state) {
+//		System.out.println("Row starts!!!!!!!! Row " + i);
+		myHeuristic.put(ROWHEAD + Integer.toString(i),new int[]{0,0});
+		int countb = 0;
+		int countw = 0;
+		char prev = '-';
+		for (int j = 0; j < 6; j ++) {
+			if (state[i][j] == 'b') {
+				if (prev == 'w' && countw > 1) {
+					sumUp(ROWHEAD, i, 1, countw);
+//					System.out.println("Should count w now");
+					countw = 0;
+				} 
+				countb ++;
+				if (j == 5) {
+					sumUp(ROWHEAD, i, 0, countb);
+//					System.out.println("Should count b now");
+					countb = 0;
+				}
+			} else if (state[i][j] == 'w') {
+				if (prev == 'b' && countb > 1) {
+					sumUp(ROWHEAD, i, 0, countb);
+//					System.out.println("Should count b now");
+					countb = 0;
+				}
+				countw ++;
+				if (j == 5) {
+					sumUp(ROWHEAD, i, 1, countw);
+//					System.out.println("Should count w now");
+					countw = 0;
+				}
+			} else {
+				if (prev == 'w') {
+					if (countw > 1){
+						sumUp(ROWHEAD, i, 1, countw);	
+					}
+					myValuew[i][j] = countw;
+//					System.out.println("Should count w now");
+				} else if (prev == 'b'){
+					if (countb > 1){
+						sumUp(ROWHEAD, i, 0, countb);
+					}
+					myValueb[i][j] = countb;
+//					System.out.println("Should count b now");
+				} 
+				countw = 0;
+				countb = 0;
+			}
+			prev = state[i][j];
+//			System.out.print(prev);
+		}
+//		System.out.println();
+		return myHeuristic.get(ROWHEAD + Integer.toString(i))[0] - myHeuristic.get(ROWHEAD + Integer.toString(i))[1];
+		
+	}
+	/**
+	 * helper method to update the heuristic value of a column
+	 * @param j col index
+	 * @param state the 2d array of current status
+	 * @return the difference between b and w on this col.
+	 */
+	private int checkCol(int j, char[][] state) {
+//		System.out.println("Cols starts!!!!!!!! Col " + j);
+		myHeuristic.put(COLHEAD + Integer.toString(j),new int[]{0,0});
+		int countb = 0;
+		int countw = 0;
+		char prev = '-';
+		for (int i = 0; i < 6; i ++) {
+			if (state[i][j] == 'b') {
+				if (prev == 'w' && countw > 1) {
+					sumUp(COLHEAD, j, 1, countw);
+//					System.out.println("Should count w now");
+					countw = 0;
+				} 
+				countb ++;
+				if (i == 5) {
+					sumUp(COLHEAD, j, 0, countb);
+//					System.out.println("Should count b now");
+					countb = 0;
+				}
+			} else if (state[i][j] == 'w') {
+				if (prev == 'b' && countb > 1) {
+					sumUp(COLHEAD, j, 0, countb);
+//					System.out.println("Should count b now");
+					countb = 0;
+				}
+				countw ++;
+				if (i == 5) {
+					sumUp(COLHEAD, j, 1, countw);
+//					System.out.println("Should count w now");
+					countw = 0;
+				}
+			} else {
+				if (prev == 'w') {
+					if (countw > 1){
+						sumUp(COLHEAD, j, 1, countw);	
+					}
+					myValuew[i][j] = countw;
+//					System.out.println("Should count w now, countw = " + countw);
+				} else if (prev == 'b'){
+					if (countb > 1){
+						sumUp(COLHEAD, j, 0, countb);
+					}
+					myValueb[i][j] = countb;
+//					System.out.println("Should count b now, countb = " + countb);
+				} 
+				countw = 0;
+				countb = 0;
+			}
+			prev = state[i][j];
+		}
+		return myHeuristic.get(COLHEAD + Integer.toString(j))[0] - myHeuristic.get(COLHEAD + Integer.toString(j))[1];
+	}
+	/**
+	 * helper method to update the heuristic value of a diagnal line
+	 * @param i row index
+	 * @param j col index
+	 * @param state the 2d array of current status
+	 * @return the difference between b and w on this diagonal line.
+	 */
+	private int checkDiag(int i, int j, char[][] state) {
+//		System.out.println("Diagnose starts!!!!!!!! I " + i + " j " + j);
+		int countb = 0;
+		int countw = 0;
+		char prev = '-';
+		int diagChoice = -1;
+		if (i == j) {
+			diagChoice = 1;
+		} else if (i + j == 5) {
+			diagChoice = 2;
+		} else {
+			return 0;
+		}
+		myHeuristic.put(DIAGHEAD + Integer.toString(diagChoice),new int[]{0,0});
+		int l = 0;
+		for (int k = 0; k < 6; k ++) {
+			if(diagChoice == 1) {
+				l = k;
+			} else {
+				l = 5 - k;
+			}
+			if (state[k][l] == 'b') {
+				myValueb[k][l] = -10;
+				if (prev == 'w' && countw > 1) {
+					sumUp(DIAGHEAD, diagChoice, 1, countw);
+//					System.out.println("Should count w now");
+					countw = 0;
+				} 
+				countb ++;
+				if (k == 5) {
+					sumUp(DIAGHEAD, diagChoice, 0, countb);
+//					System.out.println("Should count b now");
+					countb = 0;
+				}
+			} else if (state[k][l] == 'w') {
+				myValueb[k][l] = -10;
+				if (prev == 'b' && countb > 1) {
+					sumUp(DIAGHEAD, diagChoice, 0, countb);
+//					System.out.println("Should count b now");
+					countb = 0;
+				}
+				countw ++;
+				if (k == 5) {
+					sumUp(DIAGHEAD, diagChoice, 1, countw);
+//					System.out.println("Should count w now");
+					countw = 0;
+				}
+			} else {
+				if (prev == 'w') {
+					if (countw > 1){
+						sumUp(DIAGHEAD, diagChoice, 1, countw);	
+					}
+					myValuew[k][l] += countw;
+//					System.out.println("Should count w now, countw = " + countw);
+				} else if (prev == 'b'){
+					if (countb > 1) {
+						sumUp(DIAGHEAD, diagChoice, 0, countb);
+					}
+					if (diagChoice == 1 && k - countb > 0  && myValueb[k - countb][l - countb ] >= 0) {
+						myValueb[k - countb][l - countb] += countb;
+					} else if (diagChoice == 2 && k - countb > 0 && myValueb[k - countb][l + countb] >= 0) {
+						myValueb[k - countb][l + countb] += countb;
+					}
+					myValueb[k][l] += countb;
+//					System.out.println("Should count b now, countb = " + countb);
+				} 
+				countw = 0;
+				countb = 0;
+			}
+			prev = state[k][l];
+//			System.out.print(prev);
+		}
+//		System.out.println();
+		return myHeuristic.get(DIAGHEAD + Integer.toString(diagChoice))[0] - 
+				myHeuristic.get(DIAGHEAD + Integer.toString(diagChoice))[1];
+	}
+	/**
+	 * method to print out the maps
+	 */
+	public void print() {
+		System.out.println("Heursitics!!!!!!!!!"); 
+		for (String a : myHeuristic.keySet()) {
+			System.out.println(a + ": b = " + myHeuristic.get(a)[0] + " w = " + myHeuristic.get(a)[1]);
+		}
+		System.out.println("Values B!!!!!!!!!");
+		StringBuilder s = new StringBuilder();
+		for (int i = 0; i < 6; i ++) {
+			for (int j = 0; j < 6; j ++) {
+				s.append(myValueb[i][j]);
+				s.append(' ');
+			}
+			s.append('\n');
+		}
+		System.out.println("Values W!!!!!!!!!");
+		System.out.println(s.toString());
+		s = new StringBuilder();
+		for (int i = 0; i < 6; i ++) {
+			for (int j = 0; j < 6; j ++) {
+				s.append(myValuew[i][j]);
+				s.append(' ');
+			}
+			s.append('\n');
+		}
+		System.out.println(s.toString());
+	}
+	/**
+	 * helper method to sum up the total heuristic value for a specific row, 
+	 * col, or diagnose and update it into heuristic map
+	 * @param rcd row col or diagnose
+	 * @param place row/col/diagnose index
+	 * @param player whose turn
+	 * @param count the numebrs in a row
+	 */
+	private void sumUp(String rcd, int place, int player, int count) {
+		if (count == 2) {
+			myHeuristic.get(rcd + Integer.toString(place))[player] = 
+					myHeuristic.get(rcd + 
+							Integer.toString(place))[player] + TWOINAROW;
+		} else if (count == 3) {
+			myHeuristic.get(rcd + Integer.toString(place))[player] = 
+					myHeuristic.get(rcd + 
+							Integer.toString(place))[player] + THREEINAROW;
+		} else if (count == 4) {
+			myHeuristic.get(rcd + Integer.toString(place))[player] = 
+					myHeuristic.get(rcd + 
+							Integer.toString(place))[player] + FOURINAROW;
+		} else if (count >= 5) {
+			myHeuristic.get(rcd + Integer.toString(place))[player] = 
+					myHeuristic.get(rcd + 
+							Integer.toString(place))[player] + FIVEINAROW;
+		}
+//		if (rcd.equals(DIAGHEAD)) 
+//			System.out.println("\n\n" + count);
+	}
+}
---
+++ src/basics.java
@@ -0,0 +1,199 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class basics {
+	/**
+	 * the array contains 4 plates for the pentago
+	 */
+	private List<char[][]> myPlates;
+
+	/**
+	 * constructor
+	 */
+	public basics () {
+		myPlates = new ArrayList<>();
+		for (int i = 0; i < 4; i ++) {
+			myPlates.add(new char[3][3]);
+		}
+		initialize();
+	}
+	/**
+	 * method to make one move and check the situation
+	 * @param place place to put
+	 * @param plate plate to put
+	 * @param player player char
+	 * @param plater plate to rotate
+	 * @param direction direction to rotate
+	 * @return 1 if b win
+	 * 		   -1 if w win
+	 * 		   -10 if put is wrong
+	 * 		   -20 if rot is wrong
+	 * 		   TIE if the game is tie
+	 */
+	public int step(int place, int plate, char player, int plater, char direction) {
+		place = place - 1;
+		plate = plate - 1;
+		plater = plater - 1;
+		System.out.println("place = " + 
+				place + " plate = " + plate + "player = " + player + "plater = " + plater + "direction = " + direction);
+		boolean pass = place(place, plate, player);
+		if (!pass) {
+			return -10;
+		}
+		print();
+		HeuristicAndValueMap a = new HeuristicAndValueMap(getState());
+		a.print();
+		pass = pass && rotate (plater, direction);
+		if (!pass) {
+			return -20;
+		}
+		print();
+		a = new HeuristicAndValueMap(getState());
+		a.print();
+		return -10;
+	}
+
+	/**
+	 * method to put
+	 * @param place place to put
+	 * @param plate plate to put
+	 * @param player player char
+	 * @return if the move is right
+	 */
+	private boolean place(int place, int plate, char player) {
+		int i = (place) / 3;
+		int j = (place) % 3;
+		if ((myPlates.get(plate))[i][j] == '-') {
+			(myPlates.get(plate))[i][j] = player;
+			return true;
+		} else {
+			System.out.println("Sth wrong: place = " + 
+		place + " plate = " + plate + "player = " + player);
+			return false;
+		}
+	}
+	
+	/**
+	 * Method to rotate
+	 * @param plater plate to rotate
+	 * @param direction direction to rotate
+	 * @return if the move is right
+	 */
+	private boolean rotate (int plater, char direction) {
+		if (plater > -1 && plater < 4) {
+			char[][] rotated = new char[3][3];
+			char[][] current = myPlates.get(plater);
+			//		right turn
+			//		123     741    11 => 13, 12 => 23, 13 => 33
+			//		456  => 852    21 => 12, 22 => 22, 23 => 32
+			//		789     963    31 => 11, 32 => 21, 33 => 31
+			if (direction == 'R' || direction == 'r') {
+				rotated [0][2] = current [0][0];
+				rotated [1][2] = current [0][1];
+				rotated [2][2] = current [0][2];
+				rotated [0][1] = current [1][0];
+				rotated [2][1] = current [1][2];
+				rotated [0][0] = current [2][0];
+				rotated [1][0] = current [2][1];
+				rotated [2][0] = current [2][2];
+				rotated [1][1] = current [1][1];
+			} else if (direction == 'L' || direction == 'l'){
+				//			left turn
+				//		123     369    11 => 31, 12 => 21, 13 => 11
+				//		456  => 258    21 => 32, 22 => 22, 23 => 12
+				//		789     147    31 => 33, 32 => 23, 33 => 13     
+				rotated [2][0] = current [0][0];
+				rotated [1][0] = current [0][1];
+				rotated [0][0] = current [0][2];
+				rotated [2][1] = current [1][0];
+				rotated [0][1] = current [1][2];
+				rotated [2][2] = current [2][0];
+				rotated [1][2] = current [2][1];
+				rotated [0][2] = current [2][2];
+				rotated [1][1] = current [1][1];
+			} else {
+				System.out.println("there is no such an action.");
+				return false;
+			}
+			myPlates.set(plater, rotated);
+			return true;
+		} else {
+			System.out.println("there is no such plate to rotate.");
+			return false;
+		}
+		
+	}
+	
+	/**
+	 * method to initialize a new game
+	 */
+	public final void initialize() {
+		for (char[][] a : myPlates) {
+			for (int i = 0; i < 3; i ++) {
+				for (int j = 0; j < 3; j ++) {
+					a[i][j] = '-';
+				}
+			}
+		}
+	}
+	/**
+	 * helper method to get the current state of the game
+	 * @return 2d array represent the state
+	 */
+	private char[][] getState() {
+		char[][] state = new char[6][6];
+		for (int i = 0; i < 6; i ++) {
+			for (int j = 0; j < 6; j ++) {
+				if (j > -1 && j < 3) {
+					if (i < 3) {
+						state[i][j] = myPlates.get(0)[i][j];
+					} else {
+						state[i][j] = myPlates.get(2)[i-3][j];
+					}
+				} else {
+					if (i < 3) {
+						state[i][j] = myPlates.get(1)[i][j-3];
+					} else {
+						state[i][j] = myPlates.get(3)[i-3][j-3];
+					}
+				}
+			}
+		}
+		return state;
+	}
+
+	/**
+	 * method to print the state
+	 */
+	public void print() {
+		StringBuilder s = new StringBuilder();
+		for (int i = 0; i < 9; i ++) {
+			for (int j = 0; j < 9; j ++) {
+				if (i == 0 || i == 4 || i == 8) {
+					s.append('*');
+				} else if (j == 0 || j == 4 || j == 8) {
+					s.append('*');
+				} else {
+					if (j > 0 && j < 4) {
+						if (i < 4) {
+							s.append(myPlates.get(0)[i-1][j-1]);
+						} else {
+							s.append(myPlates.get(2)[i-5][j-1]);
+						}
+					} else {
+						if (i < 4) {
+							s.append(myPlates.get(1)[i-1][j-5]);
+						} else {
+							s.append(myPlates.get(3)[i-5][j-5]);
+						}
+					}
+				}
+			}
+			s.append('\n');
+		}
+		System.out.println(s.toString());
+		
+	}
+
+}
